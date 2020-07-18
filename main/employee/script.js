73d446b1ef2986b51d6e4386ff8ae00b394ef762
Sam Wu <samwu@MacBook-Pro.local>
2020-07-19 06:26:47 +1000

    finale nearly

--- main/employee/script.js
+++ main/employee/script.js
@@ -1,31 +1,36 @@
 $(function() {
+	$("#time").hide();
 	$("#popup").hide();
 
-	function startTimer(duration, display) {
-	    var timer = duration, minutes, seconds;
-	    setInterval(function () {
-	        minutes = parseInt(timer / 60, 10);
-	        seconds = parseInt(timer % 60, 10);
+	$("#start").click(function() {
+		$("#start").hide();
+		$("#time").fadeIn("fast");
+		function startTimer(duration, display) {
+		    var timer = duration, minutes, seconds;
+		    setInterval(function () {
+		        minutes = parseInt(timer / 60, 10);
+		        seconds = parseInt(timer % 60, 10);
 
-	        minutes = minutes < 10 ? "0" + minutes : minutes;
-	        seconds = seconds < 10 ? "0" + seconds : seconds;
+		        minutes = minutes < 10 ? "0" + minutes : minutes;
+		        seconds = seconds < 10 ? "0" + seconds : seconds;
 
-	        display.text(minutes + ":" + seconds);
+		        display.text(minutes + ":" + seconds);
 
-	        if (--timer < 0) {
-	            timer = duration;
-				$("#popup").fadeIn("slow");
-	        }
-	    }, 1000);
-	}
+		        if (--timer < 0) {
+		            timer = duration;
+					$("#popup").fadeIn("slow");
+		        }
+		    }, 1000);
+		}
 
-	jQuery(function ($) {
-	    var fiveMinutes = 10,
-	    display = $('#time');
-	    startTimer(fiveMinutes, display);
-	});
+		jQuery(function ($) {
+		    var fiveMinutes = 10,
+		    display = $('#time');
+		    startTimer(fiveMinutes, display);
+		});
 
-	$(".hide").click(function() {
-		$("#popup").fadeOut("slow");
+		$(".hide").click(function() {
+			$("#popup").fadeOut("slow");
+		});
 	});
 });
